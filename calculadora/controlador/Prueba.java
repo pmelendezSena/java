1fbe322ce95316de3a94d1f5216017b2bc0a9c7a
pmelendezSena <52000064+pmelendezSena@users.noreply.github.com>
2019-06-19 10:44:09 -0500

    Create Prueba.java

---
+++ calculadora/controlador/Prueba.java
@@ -0,0 +1,19 @@
+package Controlador;
+
+import vista.Interfaz;
+
+/**
+ *
+ * @author pmelendez
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Interfaz interfaz = new Interfaz();
+       interfaz.setVisible(true);
+    }
+    
+}
