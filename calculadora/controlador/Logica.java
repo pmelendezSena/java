1eb2ea3058df49cd60fa08751a8f4291dc0c8e10
pmelendezSena <52000064+pmelendezSena@users.noreply.github.com>
2019-06-19 11:30:54 -0500

    Create Logica.java

---
+++ calculadora/controlador/Logica.java
@@ -0,0 +1,21 @@
+package Controlador;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import vista.Interfaz;
+
+/**
+ *
+ * @author pmelendez
+ */
+public class Logica implements ActionListener{
+
+      public Logica(Interfaz interfaz) {
+      
+      }
+      
+       @Override
+    public void actionPerformed(ActionEvent e) {
+    
+    }
+}
+
