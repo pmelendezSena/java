e08d3070dcb7b75c9cab30ce920f47a00ccf55bd
pmelendezSena <52000064+pmelendezSena@users.noreply.github.com>
2019-06-19 08:47:48 -0500

    Update Interfaz.java

--- calculadora/vista/Interfaz.java
+++ calculadora/vista/Interfaz.java
@@ -1,3 +1,10 @@
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
 public class Interfaz extends JFrame {
     public JButton jButton0;
     public JButton jButton1;
