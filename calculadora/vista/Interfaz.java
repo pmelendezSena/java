531cf3ee0e6e3fd1a44792f33b36bbe8c633d4b2
pmelendezSena <52000064+pmelendezSena@users.noreply.github.com>
2019-06-19 08:12:09 -0500

    Create Interfaz.java

---
+++ calculadora/vista/Interfaz.java
@@ -0,0 +1,22 @@
+public class Interfaz extends JFrame {
+    public JButton jButton0;
+    public JButton jButton1;
+    public JButton jButton2;
+    public JButton jButton3;
+    public JButton jButton4;
+    public JButton jButton5;
+    public JButton jButton6;
+    public JButton jButton7;
+    public JButton jButton8;
+    public JButton jButton9;
+    public JButton jButtonSum;
+    public JButton jButtonRes;
+    public JButton jButtonMul;
+    public JButton jButtonDiv;
+    public JButton jButtonMm;
+    public JButton jButtonPunto;
+    public JButton jButtonC;
+    public JButton jButtonCE;
+    public JButton jButtonInv;
+    public JButton jButtonTot;
+}
